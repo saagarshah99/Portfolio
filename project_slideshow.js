cc8298d6576b6fae054de62aa3391d96003bfcd3
Saagar Shah <saagarshahs@hotmail.com>
2021-08-09 17:11:20 +0100

    Added video popup (lightbox) to project slideshow

--- project_slideshow.js
+++ project_slideshow.js
@@ -21,3 +21,29 @@ function showSlides(n)
   
   slides[slideIndex-1].style.display = "block";
 }
+
+//take path of video as input and load it in lightbox (video popup)
+function openProjectDemo(newSrc) 
+{
+    var lightBoxVideo = document.getElementById("project-video-demo");
+    lightBoxVideo.src = newSrc;
+    
+    window.scrollTo(0, 0);
+    document.getElementById('light').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+    //   lightBoxVideo.play();
+}
+
+//exit the lightbox if the exit button/outside fade area is clicked
+function closeProjectDemo() 
+{
+    var lightBoxVideo = document.getElementById("project-video-demo");
+    document.getElementById('light').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+    lightBoxVideo.pause();
+}
+window.document.onkeydown = function(e) 
+{
+    if (!e) {e = event;}
+    if (e.keyCode == 27) {closeProjectDemo();} //esc key
+}
