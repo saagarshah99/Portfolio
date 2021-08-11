3d004358a0346cbb1777a608ca9b47ab30e3c997
Saagar Shah <saagarshahs@hotmail.com>
2021-08-11 15:41:06 +0100

    created caption/navigation bar, further responsiveness

--- dist/project_slideshow.dev.js
+++ dist/project_slideshow.dev.js
@@ -1,54 +1,60 @@
+"use strict";
+
 //generateProjectSlides();
-
 // Next/previous controls (receive positive or negative value)
-function plusSlides(n) 
-{
-    showSlides(slideIndex += n);
-}
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+} //set default project slide value and choose which image to output depending on which index it receives
 
-//set default project slide value and choose which image to output depending on which index it receives
+
 var slideIndex = 1;
 showSlides(slideIndex);
-function showSlides(n) 
-{
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
 
-    for (var i = 0; i < slides.length; i++) 
-    {
-        slides[i].style.display = "none";
-    }
+function showSlides(n) {
+  var slides = document.getElementsByClassName("mySlides");
 
-    slides[slideIndex-1].style.display = "block";
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+} //take path of video as input and load it in lightbox (video popup)
+
+
+function openProjectDemo(newSrc) {
+  var lightBoxVideo = document.getElementById("project-video-demo");
+  lightBoxVideo.src = newSrc;
+  window.scrollTo(0, 0);
+  document.getElementById('lightbox').style.display = 'block';
+  document.getElementById('lightbox-fade').style.display = 'block'; //   lightBoxVideo.play();
+} //exit the lightbox if the exit button/outside fade area is clicked
+
+
+function closeProjectDemo() {
+  var lightBoxVideo = document.getElementById("project-video-demo");
+  document.getElementById('lightbox').style.display = 'none';
+  document.getElementById('lightbox-fade').style.display = 'none';
+  lightBoxVideo.pause();
 }
 
-//take path of video as input and load it in lightbox (video popup)
-function openProjectDemo(newSrc) 
-{
-    var lightBoxVideo = document.getElementById("project-video-demo");
-    lightBoxVideo.src = newSrc;
-    
-    window.scrollTo(0, 0);
-    document.getElementById('lightbox').style.display = 'block';
-    document.getElementById('lightbox-fade').style.display = 'block';
-    //   lightBoxVideo.play();
-}
+window.document.onkeydown = function (e) {
+  if (!e) {
+    e = event;
+  }
 
-//exit the lightbox if the exit button/outside fade area is clicked
-function closeProjectDemo() 
-{
-    var lightBoxVideo = document.getElementById("project-video-demo");
-    document.getElementById('lightbox').style.display = 'none';
-    document.getElementById('lightbox-fade').style.display = 'none';
-    lightBoxVideo.pause();
-}
-window.document.onkeydown = function(e) 
-{
-    if (!e) {e = event;}
-    if (e.keyCode == 27) {closeProjectDemo();} //esc key
-}
+  if (e.keyCode == 27) {
+    closeProjectDemo();
+  } //esc key
 
+};
 /*var projectSlidesObject = [
     ["jca_incubation.png", "JCA | London Fashion Academy Incubation Portal", 
     "https://www.w3schools.com/html/mov_bbb.mp4",
