0845542bdffe5153377d3c6ccce8aacde111836c
Saagar Shah <saagarshahs@hotmail.com>
2021-08-12 15:08:23 +0100

    Demo video is now centered and positioned correctly

--- project_slideshow.js
+++ project_slideshow.js
@@ -29,17 +29,15 @@ function openProjectDemo(newSrc)
     var lightBoxVideo = document.getElementById("project-video-demo");
     lightBoxVideo.src = newSrc;
     
-    // window.scrollTo(0, 0);
     document.getElementById('lightbox').style.display = 'block';
     document.getElementById('lightbox-fade').style.display = 'block';
-    //   lightBoxVideo.play();
-    window.scrollTo(0,document.body.scrollHeight);
+    // lightBoxVideo.play();
 }
 
 //exit the lightbox if the exit button/outside fade area is clicked
 function closeProjectDemo() 
 {
-    goToPageBottom();
+    // goToPageBottom();
     var lightBoxVideo = document.getElementById("project-video-demo");
     document.getElementById('lightbox').style.display = 'none';
     document.getElementById('lightbox-fade').style.display = 'none';
